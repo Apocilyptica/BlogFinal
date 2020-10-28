b53682335d54f4803be1ad4748586292d2fb6830
James Jager <jviper882000@gmail.com>
2020-10-28 07:38:15 -0400

    Final Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./App.css";
 import DisplayPosts from "./components/DisplayPosts";
 import CreatePost from "./components/CreatePost";
 import { withAuthenticator } from "aws-amplify-react";
+import Amplify, { Auth } from "aws-amplify";
+import "@aws-amplify/ui/dist/style.css";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
