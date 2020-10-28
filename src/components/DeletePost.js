64d4b9d5afa8d6e674fa0d4d21337a6b0a5b2aca
James Jager <jviper882000@gmail.com>
2020-10-28 06:35:27 -0400

    First Commit

---
+++ src/components/DeletePost.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { deletePost } from "../graphql/mutations";
+import { API, graphqlOperation } from "aws-amplify";
+
+class DeletePost extends Component {
+  handleDeletePost = async (postId) => {
+    const input = {
+      id: postId,
+    };
+
+    await API.graphql(graphqlOperation(deletePost, { input }));
+  };
+
+  render() {
+    const post = this.props.data;
+
+    return (
+      <button onClick={() => this.handleDeletePost(post.id)}>Delete</button>
+    );
+  }
+}
+
+export default DeletePost;
